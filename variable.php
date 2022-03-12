4e2f43c30197d97f483349775a3c537712d0ba4b
choirinnisafitria <choirinnisa356@gmail.com>
2022-03-12 11:37:57 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,23 @@
+<?php
+    //OPERATOR ARITMATIKA
+    $a = 12;
+    $b = 3;
+    $c = 5;
+    $d = 10;
+ 
+    echo "<br/>  PENJUMLAHAN <br/>";
+    echo "<br/>";
+    $f = $c+$d;
+    echo "$c + $d = $f <br />";$f;    //tercetak 15
+ 
+    echo "<br/> PENGURANGAN <br/>";
+    echo "<br/>";
+    $f = $d-(-$c);
+    echo "$d -(-$c) = $f <br/>";$f;     //tercetak 15
+    echo -$d."<br/>";     //tercetak -10
+ 
+    echo "<br/> PERKALIAN <br/>";
+    $e = ($a*$b);
+    echo "$a x $b = $e <br/>";$e;  //tercetak 36
+    echo ($b*-$c)."<br/>";    //tercetak -15
+ 
